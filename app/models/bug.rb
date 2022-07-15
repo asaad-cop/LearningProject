f43138c4222229bc6e928ebcbab4218b88cc03bc
asaad-cop <muhammad.asaad@codingcops.com>
2022-07-15 15:21:28 +0500

    Implemented 13 out of 14 features

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -6,4 +6,13 @@ class Bug < ApplicationRecord
     belongs_to :user
     belongs_to :project
     enum status: { New: 0, Started: 1, Completed: 2}
+    mount_uploader :screenshot, FileUploader
+    validate :upload_is_image
+
+    private
+    def upload_is_image
+        unless screenshot and screenshot.content_type =~ /^image\/(gif|png)$/
+            errors.add(:upload, "Not a valid image")
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2022_07_14_105923) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_type", default: 0
+    t.integer "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
